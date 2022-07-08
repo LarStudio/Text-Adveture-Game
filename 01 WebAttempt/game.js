45d39c2d369581f9b27da52e52a74d176fdc62d0
LarStudio <99085445+LarStudio@users.noreply.github.com>
2022-07-08 16:26:49 +0200

    Add files via upload

---
+++ 01 WebAttempt/game.js
@@ -0,0 +1,30 @@
+
+const textElement = document.getElementById('text')
+const optionButtonsElment = document.getElementById('option-Buttons')
+
+let state = {}
+
+function startGame() {
+    state = {}
+    showTextNode(1);
+}
+
+function showTextNode(textNodeIndex) {
+
+}
+
+function selectOption() {
+
+}
+
+const textNodes = [
+    {
+        id: 1,
+        text: 'You wake up on a bed',
+        options: [
+
+        ]
+    }
+]
+
+startGame()
